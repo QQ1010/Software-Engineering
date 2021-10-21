0100829ba8e4a79e4012fa245835a338087ad4b5
QQ <asdfg335522@gmail.com>
2021-10-21 19:41:38 +0800

    2021.10.21

--- passport-config.js
+++ passport-config.js
@@ -1,6 +1,32 @@
 // local passport
 const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+// import * as bcrypt from 'bcrypt';
 
-function initializePassword(passport) {
-    passport.use(new LocalStrategy({ usernameField: 'email'}), )
+function initialize(passport, getUserByEmail, getUserById){
+    const authenticateUser = async (email, password, done) => {
+        const user = getUserByEmail(email);
+        if(user == null) {
+            return done(null, false, {message: "No user with this email."});
+        }
+
+        try {
+            if (await bcrypt.compare(password, user.password)) {
+                return done(null, user);
+            } else {
+                return done(null, false, {message: 'Password incorrenct.'});
+            }
+        } catch(e) {
+            return done(e);
+        }
+    }
+    passport.use(new LocalStrategy({ usernameField: 'email'}, authenticateUser))
+    //序列化用戶
+    passport.serializeUser((user,done) => done(null, user.id))
+    //反序列化用戶
+    passport.deserializeUser((id,done) => { 
+        return done(null, getUserById(id))
+    });
 }
+
+module.exports = initialize
--- server.js
+++ server.js
@@ -1,34 +1,55 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 const express = require('express');
 const server = express();
 const bcrypt = require('bcrypt');
+// 暫定為本地passport => 需再更改成npm install passport-google-oauth20
 const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+
 const initializePassport = require('./passport-config')
-initializePassport(passport);
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
 
 const users = [];  // 暫時存取資料，重新整理頁面時會將資料清空 => 之後要在修改連接資料庫
 
 server.set('view-engine','ejs');
 server.use(express.urlencoded({ extend: false}));  // 索取url中的資料 for post
+server.use(flash());
+server.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+server.use(passport.initialize());
+server.use(passport.session());
 
 server.get('/', (request, response) => {
-    response.render('index.ejs', {name: 'Anila'});
+    response.render('../views/index.ejs', {name: 'Anila'});
 })
 
 server.get('/login', (request, response) => {
-    response.render('login.ejs');
+    response.render('../views/login.ejs');
 })
 
 server.get('/register', (request, response) => {
-    response.render('register.ejs');
+    response.render('../views/register.ejs');
 })
 
 // 登入請求處理
-server.post('/login', (request, response) => {
-    request.body.name
-});
+server.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true   // 顯示錯誤訊息給使用者
+}));
 
-// 註冊請求處理
-server.post('/register', (request, response) => {
+//註冊請求處理
+server.post('/register', async (request, response) => {
     try {
         const hashedPassword = await bcrypt.hash(request.body.password, 10);
         users.push( {
@@ -41,6 +62,7 @@ server.post('/register', (request, response) => {
     } catch {
         response.redirect('/register')  //註冊失敗，將使用者重新設定回註冊畫面
     }
+    console.log(users);
 });
 
 server.listen(3000);
